48b56beacee1e9e8573c3e2c20346c134f166306
suleymannmeral <mrlslymn02@gmail.com>
2025-07-18 22:04:13 +0300

    CalculatorTesting

---
+++ UnitTest/CalculatorLibrary/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)=>
+            a + b;
+
+        public int Subtract(int a, int b)=>
+            a - b;
+
+
+        public int Multiply(int a, int b)=>
+            a * b;
+
+        public int Divide(int a, int b) =>
+            a / b;
+
+    }
+}
---
+++ UnitTest/CalculatorLibraryTest/CalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using CalculatorLibrary;
+
+namespace CalculatorLibraryTests;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_ShouldAddTwoNumbers_WhenTwoNumbersAreIntegers()
+    {
+        //Arrange
+      var calculator = new Calculator();
+
+        //Act
+        var result = calculator.Add(5, 3);
+
+        //Assert
+        Assert.Equal(8, result);
+
+
+    }
+}
